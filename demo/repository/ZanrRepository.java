f16c5469fbcce5f9aff343fef4f12d6d8743a16e
MarkoKolarski <129896932+MarkoKolarski@users.noreply.github.com>
2023-06-09 13:38:56 +0200

    Delete demo/repository directory

--- demo/repository/ZanrRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.model.Zanr;
-@Repository
-public interface ZanrRepository extends JpaRepository<Zanr, Long> {
-}
